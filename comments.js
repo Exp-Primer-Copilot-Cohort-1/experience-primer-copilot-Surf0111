76c863736b0b7c1e9ad179f91b4518a906b04f5e
Surf0111 <101248306+Surf0111@users.noreply.github.com>
2023-11-05 14:23:48 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,4 +1,14 @@
 // create web server
-function doesnothing () {
-    null
-}
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, World!\n');
+});
+
+const port = 3000;
+server.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}/`);
+});
+
